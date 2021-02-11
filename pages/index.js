63ee49990b110ad268f9bc8c0d397e9774006885
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-02-11 10:05:46 -0300

    ✨ aula 1 done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
+const TitleStyle = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
-
+function Title(props) {
+  return <h1>{props.children}</h1>
+}
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div>
+      <TitleStyle><Title>Seja bem vindo</Title></TitleStyle>
+    </div>
+  )
 }
