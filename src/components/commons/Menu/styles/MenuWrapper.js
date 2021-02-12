96ccb15e7b1bb4744d3e289f1b1bcecf7db0537c
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-02-12 14:47:37 -0300

    ✨ aula4 done

--- src/components/commons/Menu/styles/MenuWrapper.js
+++ src/components/commons/Menu/styles/MenuWrapper.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const MenuWrapper = styled.nav`
-  font-family: 'Rubik', sans-serif;
+  font-family: ${({theme})=> theme.fontFamily};
   display: flex;
   align-items: center;
   justify-content: space-between;
