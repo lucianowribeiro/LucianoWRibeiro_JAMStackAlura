b3b6f0e0ea9b72ff6d1e24318678daf6a99359ce
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-03-12 12:11:30 -0300

    :construction: refactor(webpage): Part 1 of aula 4 m03 done

--- pages/index.js
+++ pages/index.js
@@ -1,112 +1,100 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Menu from '../src/components/commons/Menu';
-import Footer from '../src/components/commons/Footer';
-import Modal from '../src/components/commons/Modal';
 import Text from '../src/components/foundation/Text';
 import Button from '../src/components/commons/Button';
 import Grid from '../src/components/foundation/layout/Grid';
-import Box from '../src/components/foundation/layout/Box';
-import FormCadastro from '../src/components/pattern/FormCadastro';
+import WebPage, { WebPageContext } from '../src/components/wrappers/WebPage';
 
-export default function Home() {
-  const [isModalOpen, setModalState] = React.useState(false);
+function HomeScreen() {
+  const webPage = React.useContext(WebPageContext);
   return (
-    <Box
-      flex="1"
-      display="flex"
-      flexWrap="wrap"
-      flexDirection="column"
-      justifyContent="space-between"
-      backgroundImage="url(/images/bubbles.svg)"
-      backgroundRepeat="no-repeat"
-      backgroundPosition="bottom right"
+    <Grid.Container
+      marginTop={{
+        xs: '32px',
+        md: '75px',
+      }}
     >
-      {/*
-        [SOLID]
-        S = Single Responsability
-        O = Open Closed
-        L = Liskov Substitution
-        I = Interface Segregation
-        D = Dependency Inversion
-      */}
-      <Modal isOpen={isModalOpen} onClose={() => setModalState(false)}>
-        {(propsModal) => (
-          <FormCadastro
-            onClose={() => setModalState(false)}
-            propsModal={propsModal}
-          />
-        )}
-      </Modal>
-      <Menu onCadastrarClick={() => setModalState(true)} />
-      <Grid.Container
-        marginTop={{
-          xs: '32px',
-          md: '75px',
-        }}
-      >
-        <Grid.Row>
-          <Grid.Col
-            value={{ xs: 12, md: 5 }}
-            offset={{ xs: 0, md: 1 }}
-            display="flex"
-            alignItems="flex-start"
-            justifyContent="center"
-            flexDirection="column"
-          >
-            <Text
-              variant="title"
-              tag="h1"
-              color="tertiary.main"
-              // textAlign="right"
-              textAlign={{
-                xs: 'center',
-                md: 'left',
-              }}
-            >
-              Compartilhe momentos e conecte-se com amigos
-            </Text>
-            <Text
-              variant="paragraph1"
-              tag="p"
-              color="tertiary.light"
-              textAlign={{
-                xs: 'center',
-                md: 'left',
-              }}
-            >
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s.
-            </Text>
-
-            <Button
-              margin={{
-                xs: 'auto',
-                md: 'initial',
-              }}
-              display="block"
-              variant="primary.main"
-              onClick={() => setModalState(true)}
-            >
-              Cadastrar
-            </Button>
-          </Grid.Col>
-          <Grid.Col
-            value={{
-              xs: 12,
-              md: 6,
+      <Grid.Row>
+        <Grid.Col
+          value={{ xs: 12, md: 5 }}
+          offset={{ xs: 0, md: 1 }}
+          display="flex"
+          alignItems="flex-start"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Text
+            variant="title"
+            tag="h1"
+            color="tertiary.main"
+        // textAlign="right"
+            textAlign={{
+              xs: 'center',
+              md: 'left',
             }}
           >
-            <img
-              alt="Imagem de celular com páginas internas do projeto com o perfil do Cage"
-              style={{ display: 'block', margin: 'auto' }}
-              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
-            />
-          </Grid.Col>
-        </Grid.Row>
-      </Grid.Container>
-      <Footer />
-    </Box>
+            Compartilhe momentos e conecte-se com amigos
+          </Text>
+          <Text
+            variant="paragraph1"
+            tag="p"
+            color="tertiary.light"
+            textAlign={{
+              xs: 'center',
+              md: 'left',
+            }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s.
+          </Text>
+
+          <Button
+            margin={{
+              xs: 'auto',
+              md: 'initial',
+            }}
+            display="block"
+            variant="primary.main"
+            onClick={() => webPage.toogleModalCadastro()}
+          >
+            Cadastrar
+          </Button>
+        </Grid.Col>
+        <Grid.Col
+          value={{
+            xs: 12,
+            md: 6,
+          }}
+        >
+          <img
+            alt="Imagem de celular com páginas internas do projeto com o perfil do Cage"
+            style={{ display: 'block', margin: 'auto' }}
+            src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+          />
+        </Grid.Col>
+      </Grid.Row>
+    </Grid.Container>
+  );
+}
+export default function Home() {
+  return (
+    <WebPage
+      seoProps={{
+        headTitle: 'Home',
+      }}
+      boxProps={{
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        backgroundImage: 'url(/images/bubbles.svg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'bottom right',
+      }}
+      menuProps={{
+        display: true,
+      }}
+    >
+      <HomeScreen />
+    </WebPage>
   );
 }
---
+++ src/components/wrappers/WebPage/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from '../../commons/Footer';
+import Menu from '../../commons/Menu';
+import Modal from '../../commons/Modal';
+import Box from '../../foundation/layout/Box';
+import FormCadastro from '../../pattern/FormCadastro';
+import SEO from '../../commons/SEO';
+
+export const WebPageContext = React.createContext({
+  toogleModalCadastro: () => {},
+});
+export default function WebPage({
+  children, seoProps, boxProps, menuProps,
+}) {
+  const [isModalOpen, setModalState] = React.useState(false);
+
+  return (
+    <WebPageContext.Provider
+      value={{
+        toogleModalCadastro: () => setModalState(!isModalOpen),
+      }}
+    >
+      <SEO {...seoProps} />
+
+      <Box flex="1" display="flex" flexDirection="column" {...boxProps}>
+        <Modal isOpen={isModalOpen} onClose={() => setModalState(false)}>
+          {(propsModal) => (
+            <FormCadastro
+              onClose={() => setModalState(false)}
+              propsModal={propsModal}
+            />
+          )}
+        </Modal>
+        {menuProps.display && <Menu onCadastrarClick={() => setModalState(true)} />}
+        {children}
+        <Footer />
+      </Box>
+    </WebPageContext.Provider>
+  );
+}
+WebPage.defaultProps = {
+  seoProps: {},
+  boxProps: {},
+  menuProps: {
+    display: true,
+  },
+};
+
+WebPage.propTypes = {
+  seoProps: PropTypes.shape({
+    headTitle: PropTypes.string,
+  }),
+  menuProps: PropTypes.shape({
+    display: PropTypes.bool,
+  }),
+  boxProps: PropTypes.shape({
+    backgroundImage: PropTypes.string,
+    backgroundRepeat: PropTypes.string,
+    backgroundPosition: PropTypes.string,
+  }),
+  children: PropTypes.node.isRequired,
+};
