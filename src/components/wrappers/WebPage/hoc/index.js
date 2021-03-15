94218c6f4c34059def6bfa8ce709cb2da4349352
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-03-15 10:26:22 -0300

    :sparkles: feat(faq): Aula 5 m03 done

--- src/components/wrappers/WebPage/hoc/index.js
+++ src/components/wrappers/WebPage/hoc/index.js
@@ -1,13 +1,18 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
 import WebPage from '..';
 import WebGlobalProvider from '../provider';
 
-export default function webPageHOC(PageComponent, { pageProps }) {
+export default function webPageHOC(PageComponent, { pageProps } = { pageProps: {} }) {
   return (props) => (
     <WebGlobalProvider>
-      <WebPage {...pageProps}>
+      <WebPage
+        {...pageProps}
+        {...props.pageProps}
+      >
         <PageComponent {...props} />
       </WebPage>
     </WebGlobalProvider>
