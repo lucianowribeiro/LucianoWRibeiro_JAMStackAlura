4dc2f6a192369723b16599c072ca04321b90eaf3
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-04-01 14:17:10 -0300

    :white_check_mark: test(login): Aula 7 m04 done

--- src/components/pattern/FormLogin/index.js
+++ src/components/pattern/FormLogin/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import * as yup from 'yup';
+import PropTypes from 'prop-types';
 import Button from '../../commons/Button';
 import TextField from '../../forms/TextField';
 import useForm from '../../../infra/hooks/form/UseForm';
@@ -17,21 +18,30 @@ const loginSchema = yup.object().shape({
     .min(8, 'Sua senha precisa ter ao menos 8 caracteres'),
 });
 
-export default function FormLogin() {
+export default function FormLogin({ onSubmit }) {
   const router = useRouter();
   const initialValues = {
     usuario: '',
     senha: '',
   };
+
   const form = useForm({
     initialValues,
     onSubmit: (values) => {
+      form.setIsFormDisabled(true);
       loginService.login({
         username: values.usuario, // 'omariosouto'
         password: values.senha, // 'senhasegura'
       })
         .then(() => {
           router.push('/app/profile');
+        })
+        .catch((err) => {
+          // Desafio: Mostrar o erro na tela
+          console.error(err);
+        })
+        .finally(() => {
+          form.setIsFormDisabled(false);
         });
     },
     async validateSchema(values) {
@@ -42,7 +52,7 @@ export default function FormLogin() {
   });
 
   return (
-    <form id="formCadastro" onSubmit={form.handleSubmit}>
+    <form id="formCadastro" onSubmit={onSubmit || form.handleSubmit}>
       <TextField
         placeholder="Usuário"
         name="usuario"
@@ -78,3 +88,10 @@ export default function FormLogin() {
     </form>
   );
 }
+FormLogin.defaultProps = {
+  onSubmit: undefined,
+};
+
+FormLogin.propTypes = {
+  onSubmit: PropTypes.func,
+};
