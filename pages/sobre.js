fbbfaa147f47a63002f71f9401afdc8bb8c05726
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-04-07 10:38:31 -0300

    :sparkles: feat(sobre): Aula3 m05 done

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,28 +1,8 @@
-import { GraphQLClient, gql } from 'graphql-request';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 import webPageHOC from '../src/components/wrappers/WebPage/hoc';
 
 export async function getStaticProps() {
-  const TOKEN = '4aab00ab314cc37b5adec40c740c86';
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-
-  const query = gql`
-    query {
-      pageSobre {
-        sobreTitle,
-        sobreDescription,
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
-
+  const messages = await getContent();
   return {
     props: {
       messages,
